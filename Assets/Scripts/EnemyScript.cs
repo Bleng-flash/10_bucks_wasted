7d55875028cced424eef279973b142dd94186f4b
Juggernaut102 <160118991+Juggernaut102@users.noreply.github.com>
2025-06-01 04:00:56 +0800

    Added Enemy script

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class EnemyScript : Entity
+{
+    [SerializeField] // just for easier testing in editor
+    private float maxHP;
+    [SerializeField]
+    private float HP;
+    [SerializeField]
+    private float ATK;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void Die()
+    {
+        Destroy(gameObject);
+    }
+}
