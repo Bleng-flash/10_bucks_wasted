46b7984aa60d701963c56e732f3fa8ef8d0bfde5
Juggernaut102 <160118991+Juggernaut102@users.noreply.github.com>
2025-06-01 01:49:18 +0800

    Added AOEPunch

---
+++ Assets/Scripts/AOEPunch.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class AOEPunch : Attack
+{
+    [SerializeField]
+    private float cooldown = 2.0f; // in seconds
+
+    [SerializeField]
+    private float damage = 10;
+    private List<Collider2D> hitEnemies = new List<Collider2D>();
+
+    public AOEPunch(float cooldown, float damage) : base(cooldown, damage, true)
+    {
+        this.cooldown = cooldown;
+        this.damage = damage;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        // For now we use Enemy tags, but see if there's a better way to classify enemies
+        // that doesn't rely on tags
+        if (collision.CompareTag("Enemy"))
+        {
+            hitEnemies.Add(collision);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        hitEnemies.Remove(collision);
+    }
+
+    // AOE Punch is an auto-attack so CanAttack is always true
+    protected override bool CanAttack()
+    {
+        return true;
+    }
+
+    protected override void PerformAttack()
+    {
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(damage);
+        }
+    }
+    
+}
