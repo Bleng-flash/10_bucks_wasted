78ba1a25b7c5513f0ed14fc4c09a8cc814d0228f
Juggernaut102 <160118991+Juggernaut102@users.noreply.github.com>
2025-06-02 02:18:43 +0800

    Add enemy bash attack logic

---
+++ Assets/Scripts/EnemyBashAttack.cs
@@ -0,0 +1,48 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+// Simple attack that deals damage when enemy comes into contact with player
+
+public class EnemyBashAttack : Attack
+{
+    private bool playerInRange = false;
+    private PlayerScript player;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Initialise(this.cooldown, this.damage, true);
+    }
+
+    protected override bool CanAttack()
+    {
+        return true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Collision!");
+            playerInRange = true;
+            player = collision.gameObject.GetComponent<PlayerScript>();
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+
+    protected override void PerformAttack()
+    {
+        Debug.Log("Perform Attack: PlayerInRange = " + playerInRange);
+        if (playerInRange)
+        {
+            Debug.Log("Hit Player for " + damage + " damage!");
+            player.TakeDamage(this.damage);
+        }
+    }
+}
