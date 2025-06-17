da19d9938be4103c7b84ecd5bcb23fbbaa9c2d7f
Juggernaut102 <160118991+Juggernaut102@users.noreply.github.com>
2025-06-18 01:09:33 +0900

    Add DropXp method call to Die method

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 // Enemy inherits from entity, contains hp and atk
 public class EnemyScript : Entity
 {
-
-
+    [SerializeField] private int xpAmount;
+    [SerializeField] private XpSpawner xpSpawner;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,6 +22,7 @@ public class EnemyScript : Entity
     {
         Destroy(gameObject);
         ScoreManager.Instance.AddScore(1);
+        xpSpawner.DropXp(xpAmount, transform.position);
         Debug.Log("Enemy killed!");
     }
 }
