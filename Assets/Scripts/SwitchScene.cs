5abeae7c02403d4ef1bab2ae8a75af18f922c298
Bryan Leng <bryan.leng33@gmail.com>
2025-06-18 16:54:36 +0800

    Add gameover scene and script to change scene

--- Assets/Scripts/SwitchScene.cs
+++ Assets/Scripts/SwitchScene.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartGame : MonoBehaviour
+public class SwitchScene : MonoBehaviour
 {
     public void LoadGameScene()
     {
         SceneManager.LoadScene("SampleScene"); // Match the scene name exactly
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene("StartScene"); // match scene name exactly
     }
 }
