7959f75e399ce90363ac241179ce0a11d635fdb5
Juggernaut102 <160118991+Juggernaut102@users.noreply.github.com>
2025-06-24 23:35:35 +0900

    Remove logic in Nonautoattack, move concrete implementation to various enemy attacks

--- Assets/Scripts/Attack/NonAutoAttack.cs
+++ Assets/Scripts/Attack/NonAutoAttack.cs
@@ -3,13 +3,6 @@ using UnityEngine;
 public abstract class NonAutoAttack : Attack
 {
     // DO NOT provide an implementation for Update(), game will autocall Attack's implementation
-
-    protected override bool TargetInRange()
-    {
-        return FindValidTarget() != null;
-    }
-
-    // We will use the field owner in Attack class and the method IsEnemyTo in Entity class
-    protected abstract Entity FindValidTarget();
+    // All enemy attacks inherit from NonAutoAttack, implements TargetInRange() method.
 
 }
