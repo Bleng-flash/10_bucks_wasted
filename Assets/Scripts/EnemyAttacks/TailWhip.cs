94c3b0b611159ae9adc441ebe618a089d86a2274
Juggernaut102 <160118991+Juggernaut102@users.noreply.github.com>
2025-06-24 23:54:39 +0900

    Implement TargetInRange() for tailwhip

--- Assets/Scripts/EnemyAttacks/TailWhip.cs
+++ Assets/Scripts/EnemyAttacks/TailWhip.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 public class TailWhip : NonAutoAttack
 {
     [SerializeField] private float attackRadius = 1.0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        Initialise(this.cooldown, this.damage);
     }
 
     // TailWhip is similar to AOEpunch, just 360 degrees 
@@ -18,16 +19,18 @@ public class TailWhip : NonAutoAttack
         PlayerScript player = playerObject.GetComponent<PlayerScript>();
             if (player != null)
             {
-                Debug.Log($"Hit {collider.name} with {this.damage} damage!");
+                Debug.Log($"Hit {playerObject.name} with {this.damage} damage!");
                 player.TakeDamage(damage);
             }
             else
             {
-                Debug.LogWarning("PlayerScript missing on: " + collider.name);
+                Debug.LogWarning("PlayerScript missing on: " + playerObject.name);
             }
     }
 
     protected override bool TargetInRange()
     {
+        Collider2D playerObject = Physics2D.OverlapCircle(transform.position, attackRadius, targetLayer);
+        return playerObject != null;
     }
 }
