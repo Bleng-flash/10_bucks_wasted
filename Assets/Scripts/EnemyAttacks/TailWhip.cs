74a02f3ec3be2215c08416c614df6fae683d6d83
Juggernaut102 <160118991+Juggernaut102@users.noreply.github.com>
2025-06-24 23:43:06 +0900

    Change OverlapCircleAll method to OverlapCircle

--- Assets/Scripts/EnemyAttacks/TailWhip.cs
+++ Assets/Scripts/EnemyAttacks/TailWhip.cs
@@ -2,23 +2,20 @@ using UnityEngine;
 
 public class TailWhip : NonAutoAttack
 {
-    [SerializeField] private float attackRadius = 2.0f;
+    [SerializeField] private float attackRadius = 1.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // TailWhip is similar to AOEpunch, just 360 degrees 
     protected override void PerformAttack()
     {
-        // Detects player (Physics2D.OverlapCircleAll returns a Collider2D array)
-        Collider2D[] players = Physics2D.OverlapCircleAll(transform.position, attackRadius, targetLayer);
+        // Detects player
+        Collider2D playerObject = Physics2D.OverlapCircle(transform.position, attackRadius, targetLayer);
 
-        // Should only have 1 player at most
-        foreach (Collider2D collider in players)
-        {
-            PlayerScript player = collider.GetComponent<PlayerScript>();
+        PlayerScript player = playerObject.GetComponent<PlayerScript>();
             if (player != null)
             {
                 Debug.Log($"Hit {collider.name} with {this.damage} damage!");
@@ -28,6 +25,9 @@ public class TailWhip : NonAutoAttack
             {
                 Debug.LogWarning("PlayerScript missing on: " + collider.name);
             }
-        }
+    }
+
+    protected override bool TargetInRange()
+    {
     }
 }
