a72e9365ca282131f1bcee59051db6cfffe7cec5
Bryan Leng <bryan.leng33@gmail.com>
2025-06-26 17:34:38 +0800

    Sync upgrade cards UI with logic and make upgrade card prefab

--- Assets/Scripts/UpgradeManager.cs
+++ Assets/Scripts/UpgradeManager.cs
@@ -1,6 +1,8 @@
 
 using UnityEngine;
+using UnityEngine.UI;
 using System;
+using System.Collections.Generic;
 
 // Upon every level up of the player, the game freezes and 3 upgrade cards appear on the screen (UI)
 // the player will pick 1 of the 3 upgrades
@@ -8,8 +10,13 @@ using System;
 public class UpgradeManager : MonoBehaviour
 {
     // This callback will be called when upgrade is finished
-    private Action onUpgradeComplete; // Action is a type that represents a method that returns void
     [SerializeField] private GameObject upgradeUI; // Reference to the upgrade UI panel
+    [SerializeField] private List<Upgrade> allUpgrades; // list of upgrades available
+    [SerializeField] private GameObject cardPrefab;
+    [SerializeField] private Transform cardContainer;
+    private Action onUpgradeComplete; // Action is a type that represents a method that returns void
+    private Upgrade selectedUpgrade;
+    private List<GameObject> activeCards = new(); // the 3 upgrade cards that player can pick from
 
     private void Awake()
     {
@@ -23,23 +30,88 @@ public class UpgradeManager : MonoBehaviour
     public void ShowUpgradeScreen(Action onComplete)
     {
         onUpgradeComplete = onComplete;
-        if (upgradeUI != null)
+        if (upgradeUI == null)
         {
-            GameManager.Instance.PauseGame();
-            upgradeUI.SetActive(true);
+            return;
+        }
+        GameManager.Instance.PauseGame();
+        upgradeUI.SetActive(true);
+        selectedUpgrade = null;
+        DisplayUpgradeOptions();
+    }
+
+    private void DisplayUpgradeOptions()
+    {
+        ClearOldCards();
+        List<Upgrade> options = GetRandomUpgrades(3);
+        foreach (Upgrade upgrade in options)
+        {
+            GameObject card = Instantiate(cardPrefab, cardContainer);
+            SetupCardUI(card, upgrade);
+            activeCards.Add(card);
+        }
+
+    }
+
+    private List<Upgrade> GetRandomUpgrades(int count)
+    {
+        List<Upgrade> pool = new(allUpgrades);
+        List<Upgrade> selected = new();
+
+        while (selected.Count < count && pool.Count > 0)
+        {
+            int index = UnityEngine.Random.Range(0, pool.Count);
+            selected.Add(pool[index]);
+            pool.RemoveAt(index);
+        }
+
+        return selected;
+    }
+
+
+    private void SetupCardUI(GameObject card, Upgrade upgrade)
+    {
+        card.transform.Find("Title").GetComponent<Text>().text = upgrade.upgradeName;
+        card.transform.Find("Description").GetComponent<Text>().text = upgrade.description;
+        card.transform.Find("Icon").GetComponent<Image>().sprite = upgrade.icon;
+
+        Button button = card.GetComponent<Button>();
+        button.onClick.AddListener(() =>
+        {
+            selectedUpgrade = upgrade;
+            HighlightCard(card);
+        });
+    }
+
+
+    private void HighlightCard(GameObject selectedCard)
+    {
+        foreach (GameObject card in activeCards)
+        {
+            card.GetComponent<Image>().color = (card == selectedCard) ? Color.yellow : Color.white;
         }
     }
 
-    // Called when the player confirms the upgrade and closes the screen
-    public void CompleteUpgrade()
+    private void ClearOldCards()
     {
-        if (upgradeUI != null)
+        foreach (GameObject card in activeCards)
         {
-            upgradeUI.SetActive(false);
+            Destroy(card);
         }
-        onUpgradeComplete?.Invoke();
-        GameManager.Instance.ResumeGame();
+        activeCards.Clear();
     }
+    
+    public void ConfirmSelection(PlayerScript player)
+    {
+        if (selectedUpgrade != null)
+        {
+            selectedUpgrade.ApplyUpgrade(player);
+            upgradeUI.SetActive(false); // exit UI screen
+            GameManager.Instance.ResumeGame();
+            onUpgradeComplete?.Invoke();
+        }
+    }
+
 
     // Implement upgrade logic here (e.g., selecting a card, applying upgrades, etc.)
 }
