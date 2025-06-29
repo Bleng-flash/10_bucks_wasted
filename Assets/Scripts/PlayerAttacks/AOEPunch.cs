5478df189c5857a64b54b9f47c1a7c4ab8d665a2
Juggernaut102 <160118991+Juggernaut102@users.noreply.github.com>
2025-06-30 04:18:51 +0800

    Merge visual/punch

--- Assets/Scripts/PlayerAttacks/AOEPunch.cs
+++ Assets/Scripts/PlayerAttacks/AOEPunch.cs
@@ -7,19 +7,40 @@ public class AOEPunch : AutoAttack
     [SerializeField] private float attackRadius = 2.0f;
     [SerializeField] private float attackAngle = 90f;       // Half-angle — so 90 = 180° cone
     private PlayerMovement playerMovement;
+    [SerializeField] private Animator punchAnimator;
+    [SerializeField] private PunchOverlay punchOverlay;
+    private Vector2 originalOffset;
 
     void Start()
     {
         // Debug.Log($"AOEPunch Start() — Cooldown: {this.cooldown}, Damage: {this.damage}");
         Initialise(this.cooldown, this.damage);
         playerMovement = GetComponentInParent<PlayerMovement>();
+        originalOffset = punchOverlay.transform.localPosition;
     }
 
     protected override void PerformAttack()
     {
         Vector2 attackOrigin = transform.position;
-        Vector2 attackDirection = playerMovement.FacingDirection;     // This will point to wherever player is facing
+        Vector2 attackDirection = playerMovement.FacingDirection.normalized;     // This will point to wherever player is facing
         if (attackDirection == Vector2.zero) attackDirection = Vector2.right;   // Failsafe if vector2 is zero
+
+        // Plays punch animation
+        if (punchAnimator != null)
+        {
+            // Rotate animation to face attack direction, while keeping offset in front of player
+            float angle = Mathf.Atan2(attackDirection.y, attackDirection.x) * Mathf.Rad2Deg;
+            Vector2 rotatedOffset = Quaternion.Euler(0, 0, angle) * originalOffset;
+            punchOverlay.transform.localPosition = rotatedOffset;
+            punchOverlay.transform.localRotation = Quaternion.Euler(0, 0, angle);
+
+            punchOverlay.PrepareSlashAnimation();
+            punchAnimator.Play("slash attack", 0, 0f);
+        }
+        else
+        {
+            Debug.Log("No punchAnimator found!");
+        }
 
         // Detects all objects will colliders that are in enemy layer and add them to hitColliders
         Collider2D[] hitColliders = Physics2D.OverlapCircleAll(attackOrigin, attackRadius, targetLayer);
