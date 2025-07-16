398570fc90f4e5bbee0dba303d0a18358b51a85c
Juggernaut102 <160118991+Juggernaut102@users.noreply.github.com>
2025-07-17 05:13:25 +0800

    Add lightning strike script and logic

--- Assets/Scripts/PlayerAttacks/LightningStrike.cs
+++ Assets/Scripts/PlayerAttacks/LightningStrike.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
 
-public class LightningStrike : MonoBehaviour
+// Lightning Strike is a prefab of a single, self-contained lightning strike, which will be used
+// by Lightning Attack (which is the actual attack consisting of many lightning strike prefabs)
+public class LightningStrike : AutoAttack
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float radius = 2f;
+
+    // Used as an animation event to deal damage in a specific frame
+    public void DoDamage()
     {
-        
+        // Detects all objects will colliders that are in enemy layer and add them to hitColliders
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, radius, targetLayer);
+        foreach (Collider2D collider in hitColliders)
+        {
+            EnemyScript enemy = collider.GetComponent<EnemyScript>();
+            if (enemy != null)
+            {
+                Debug.Log($"Lightning Strike {collider.name} with {this.damage} damage!");
+                enemy.TakeDamage(damage);
+            }
+            else
+            {
+                Debug.LogWarning("EnemyScript missing on: " + collider.name);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Used as an animation event in the last frame of animation to delete lightning strike
+    public void DestroySelf()
     {
-        
+        Destroy(gameObject);
+    }
+
+    protected override void PerformAttack()
+    {
+        return;
+    }
+    public override void Recalculate()
+    {
+        return;
     }
 }
