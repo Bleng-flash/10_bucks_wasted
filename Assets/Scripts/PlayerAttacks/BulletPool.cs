97105826e122cb6f343e56030e93e4938e2be83e
Juggernaut102 <160118991+Juggernaut102@users.noreply.github.com>
2025-07-20 06:08:29 +0800

    Make pooled objects be created as children of poolmanager gameobject

--- Assets/Scripts/PlayerAttacks/BulletPool.cs
+++ Assets/Scripts/PlayerAttacks/BulletPool.cs
@@ -12,7 +12,7 @@ public class BulletPool : MonoBehaviour
     {
         for (int i = 0; i < poolSize; i++)
         {
-            GameObject bullet = Instantiate(bulletPrefab);
+            GameObject bullet = Instantiate(bulletPrefab, transform);
             bullet.SetActive(false);
             bulletPool.Enqueue(bullet);
         }
